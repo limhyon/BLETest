376b336a2865726cafa6e3c8e7f95b275b387b26
snu <snu@snuui-MacBook-Pro.local>
2014-10-15 01:33:24 +0900

    Initial Commit

---
+++ BLEtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLEtest
+//
+//  Created by snu on 2014. 10. 15..
+//  Copyright (c) 2014년 snu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BLEtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLEtest
+//
+//  Created by snu on 2014. 10. 15..
+//  Copyright (c) 2014년 snu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
